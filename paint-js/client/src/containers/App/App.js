a5bff830539272e5ae1e1f6bfe97b5ebc669b810
ad-PC\ad <jnole94@gmail.com>
2018-12-02 13:28:25 +0100

    remove Puesta a punto

---
+++ paint-js/client/src/containers/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Layout from '../Layout/Layout';
+import './App.css';
+
+
+class App extends Component {
+  /**
+   * Render this Component on each state change
+   * @return {React.Component}
+   */
+  render() {
+    return (
+      <Layout />
+    );
+  }
+}
+
+export default App;
