5b3f1480ab00942bb873e8fb1e5e28f574d80b4b
ad-PC\ad <jnole94@gmail.com>
2018-12-10 19:23:24 +0100

    Layout changes

--- paint-js/src/components/DoUndo.js
+++ paint-js/src/components/DoUndo.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import { Stage, Layer, Line } from "react-konva";
-import { Card } from 'antd';
+import { Card, Icon } from 'antd';
 
 class DoUndo extends Component {
 
     render() {
       return (
         <Card>
-           <p>  DO UNDO</p>
-         </Card>
+           <Icon type="undo"  theme="outlined" style={{ "fontSize": '2.5em' }}/>
+           <Icon type="redo"  theme="outlined" style={{ "fontSize": '2.5em' }}/>
+        </Card>
       );
     }
 }
